57a36f7bcdae1967a3ccaf89ca10c36d71e07d7f
IxLahiji <ixabat16@gmail.com>
2018-04-05 17:39:58 -0600

    Added random features from old bot, and started skeleoton for trello reminders

--- trelloNotificationBot/trelloNotificationBot/Model/ConfigTemplates/Tokens.cs
+++ trelloNotificationBot/trelloNotificationBot/Model/ConfigTemplates/Tokens.cs
@@ -6,11 +6,13 @@ namespace trelloNotificationBot.Model.ConfigTemplates
     {
         public string discord { get; set; }
         public string giphy { get; set; }
+        public string trello { get; set; }
 
-        public Tokens(string discord = null, string giphy = null)
+        public Tokens(string discord = null, string giphy = null, string trello = null)
         {
             this.discord = discord;
             this.giphy = giphy;
+            this.trello = trello;
         }
     }
 }
---
+++ trelloNotificationBot/trelloNotificationBot/Services/Reminder.cs
@@ -0,0 +1,31 @@
+﻿using Discord;
+using Discord.WebSocket;
+using TrelloNet;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace trelloNotificationBot.Services
+{
+    class Reminder
+    {
+        private readonly DiscordSocketClient discord;
+        private readonly IConfigurationRoot config;
+        private readonly ITrello trello;
+
+        // DiscordSocketClient and CommandService are injected automatically from the IServiceProvider
+        public Reminder(DiscordSocketClient discord, IConfigurationRoot config)
+        {
+            this.discord = discord;
+            this.config = config;
+            this.trello = new Trello(this.config["tokens:trello"]);
+        }
+
+
+        public async Task StartAsync()
+        {
+            // get board info, sleep until next morning.
+        }
+    }
+}
